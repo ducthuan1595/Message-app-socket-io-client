87cdccdfe0c662742e433a71e3712279f9b7bb23
thuantruong <hi.tim.gi.the@gmail.com>
2023-10-15 22:58:50 +0700

    fix protect rount

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -6,10 +6,13 @@ import ChatsPage from "./pages/Chats";
 import FormPage from "./pages/Form";
 import CheckOutlet from "./config/checkOutlet";
 import "./App.css";
+import { ChatState } from "./store/ChatProvider";
 
 function App() {
+  const { user, token } = ChatState();
   return (
     <div className="App">
+      {!user && !token && <Navigate to="/login" replace={true} />}
       <Routes>
         <Route path="/login" element={<FormPage />} />
         <Route element={<CheckOutlet />}>
